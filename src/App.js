2523f2d1064d54548272965b4baa80d3c68f2819
bharath <karla.bharathnandan@gmail.com>
2024-02-11 20:43:45 -0800

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { IDKitWidget } from '@worldcoin/idkit';
+
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div>
+      <IDKitWidget
+        app_id="app_staging_2ac5c88560df081461fdf46b8ea12921" // Replace with your actual app_id
+        action="vote"
+        signal=""
+        action_description="cast your vote"
+        onError={onError}
+        handleVerify={handleVerify}
+        verification_level="device"
+      >
+        {({ open }) => <button onClick={open}>Verify with World ID</button>}
+      </IDKitWidget>
+    </div>
       </header>
     </div>
   );
+}
+
+function onError(data) {
+  console.error('error:', data);
+  // Handle verified user data here
+}
+
+
+
+function handleVerify(data) {
+  console.log('Verification handler:', data);
+  
 }
 
 export default App;
